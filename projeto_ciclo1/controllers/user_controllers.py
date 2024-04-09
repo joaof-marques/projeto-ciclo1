0d243670dae4c0afabeb1d5b4fb2345cceb61ad8
João Marques <joaofmarques.dev@gmail.com>
2024-04-09 02:28:31 -0300

    Adiciona encriptação com bcrypt hash salt

--- projeto_ciclo1/controllers/user_controllers.py
+++ projeto_ciclo1/controllers/user_controllers.py
@@ -1,17 +1,20 @@
 from database.database import User, engine
 from sqlalchemy.orm import Session
-import bcrypt
+from controllers.system_log_controllers import insert_system_log
+import bcrypt, time
 
 def createUser(name, email, cpf, password, access_level):
     
-    with Session(bind=engine) as session:
-        
-        try:
-            novo_usuario = User(name = name, email = email, cpf = cpf, password=password, access_level = access_level)
+    with Session(bind=engine) as session:        
+        try:            
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            
+            novo_usuario = User(name = name, email = email, cpf = cpf, password=hashed_password, access_level = access_level)
             
             session.add(novo_usuario)
             session.commit()
             return True, f"Usuário {name} cadastrado com Sucesso!"
         except Exception as error:
             session.rollback()
+            log_insert_status, log_insert_message = insert_system_log(error)            
             return False, f"Usuário {name} não pôde ser cadastrado.\n Erro {error}"
