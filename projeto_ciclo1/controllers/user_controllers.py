45ec09f26e7861ced3ab8ae832e2a4ff32c0bfa5
João Marques <joaofmarques.dev@gmail.com>
2024-04-09 18:16:29 -0300

    Remove print que ficou de teste no código

--- projeto_ciclo1/controllers/user_controllers.py
+++ projeto_ciclo1/controllers/user_controllers.py
@@ -8,7 +8,6 @@ def create_user(name, email, cpf, password, access_level):
     with Session(bind=engine) as session:        
         try:
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            print(hash)
             novo_usuario = User(name = name, email = email, cpf = cpf, password=str(hashed_password.decode('utf-8')), access_level = access_level)
             
             session.add(novo_usuario)
