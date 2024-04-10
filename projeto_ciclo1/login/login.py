5e615abc3e2a1613d894291e655417840892fea4
João Victor <jv.sena0@gmail.com>
2024-04-10 11:31:21 -0300

    Adicionado a leitura de senha encriptada na autenticação de login do usuario

--- projeto_ciclo1/login/login.py
+++ projeto_ciclo1/login/login.py
@@ -1,43 +1,41 @@
 import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import User # Caminho para a tabela User da classe SQLAlchemy
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# DB_USER, DB_PASSWORD, DB_HOST, DB_NAME = os.getenv('DB_USER'), os.getenv('DB_PASSWORD'), os.getenv('DB_HOST'), os.getenv('DB_NAME')
-
-
-# URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+from database import User
+import bcrypt
 
 def authenticate(email, password):
+   
     engine = create_engine('postgresql+psycopg2://postgres:12345@localhost/desafio')  
     Session = sessionmaker(bind=engine)
     session = Session()
-    user = session.query(User).filter_by(email=email, password=password).first()
+    user = session.query(User).filter_by(email=email).first()
     session.close()
-    return user
 
+    if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+        return True, user
+    else:
+        return False, None
 
-def login_page():
-    st.title('Login')
+def login_auth():
+    st.write('Cadastro')
+        
     email = st.text_input('Email')
-    password = st.text_input('Password', type='password')
-    if st.button('Login'):
-        user = authenticate(email, password)
+    password = st.text_input('Senha', type='password')
+
+    if st.button('Enviar'):
+        _, user = authenticate(email, password)
+        print(user)
         if user:
             st.success(f'Logged in as {user.name}')
-            return True
+            return True, user
         else:
             st.error('Invalid email or password')
-            return False
-
+            return False, None
 
 
 def main():
-    login_page()
+    login_auth()
 
 if __name__ == '__main__':
     main()
