90bd1e5bd056efed4de13353c0ec7708b1c37259
João Victor <jv.sena0@gmail.com>
2024-04-10 11:44:02 -0300

    Adição das variáveis do arquivo .env no arquivo de autenticação de login

--- projeto_ciclo1/login/login.py
+++ projeto_ciclo1/login/login.py
@@ -3,10 +3,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database import User
 import bcrypt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER, DB_PASSWORD, DB_HOST, DB_NAME = os.getenv('DB_USER'), os.getenv('DB_PASSWORD'), os.getenv('DB_HOST'), os.getenv('DB_NAME')
+
+
+URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
 
 def authenticate(email, password):
    
-    engine = create_engine('postgresql+psycopg2://postgres:12345@localhost/desafio')  
+    engine = create_engine(URL)  
     Session = sessionmaker(bind=engine)
     session = Session()
     user = session.query(User).filter_by(email=email).first()
