2e6dcb9a9ff673703029292d8c3bed4269e56b43
João <joaofmarques.dev@gmail.com>
2024-04-14 23:14:25 -0300

    Alters the function create_user to insert logs into the LogUser table

--- projeto_ciclo1/controllers/user_controllers.py
+++ projeto_ciclo1/controllers/user_controllers.py
@@ -1,18 +1,23 @@
-from database.database import User, engine
+from database.database import User, LogUser, engine
 from sqlalchemy.orm import Session
 from controllers.system_log_controllers import insert_system_log
 import bcrypt
 
-def create_user(name, email, cpf, password, access_level):
+def create_user(name, email, cpf, password, access_level, logged_user_id):
     
     with Session(bind=engine) as session:        
         try:
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            novo_usuario = User(name = name, email = email, cpf = cpf, password=str(hashed_password.decode('utf-8')), access_level = access_level)
+            new_user = User(name = name, email = email, cpf = cpf, password=str(hashed_password.decode('utf-8')), access_level = access_level, deleted=False)
             
-            session.add(novo_usuario)
+            session.add(new_user)
             session.commit()
-            return True, novo_usuario
+            
+            new_log = LogUser(id_user_modifier=logged_user_id, id_user_modified=new_user.id, log_txt="New User created.")
+            
+            session.add(new_log)
+            session.commit()
+            return True, new_user
         except Exception as error:
             session.rollback()
             log_insert_status, log_insert_message = insert_system_log(error)            
