1f3fc231449824e32e262087419f48c86b575b40
João Marques <joaofmarques.dev@gmail.com>
2024-04-24 14:24:24 -0300

    Adds the content field to the document details section

--- projeto_ciclo1/controllers/documents_controllers.py
+++ projeto_ciclo1/controllers/documents_controllers.py
@@ -1,58 +1,113 @@
-from database.database import Document, engine
+from database.database import Document, User, LogDocument, engine
 from sqlalchemy.orm import Session
-from projeto_ciclo1.controllers.logs_controllers import insert_system_log
+# from projeto_ciclo1.controllers.system_log_controllers import insert_system_log
 from time import time
-import os
+import datetime, json
 
-def create_document(new_file, user_id, tags:list=[]):
-    relative_path = save_document_get_path(new_file)
-    
-    # change this to the OCR method when ready
-    content = new_file.getvalue().decode("utf-8")
 
-    with Session(bind=engine) as session:
-        try:           
-            new_document = Document(name=new_file.name, type=new_file.type, id_register_user=user_id, img=relative_path, tags=tags, content=content, deleted=False)
+class DocumentControllers:
+
+    @classmethod
+    def get_query_lenght(self, file_name, user_register_name, starting_date=None, limit_date=None):
+        
+        if starting_date is None:
+            starting_date = datetime.datetime(1900, 1, 1)
+        if limit_date is None:
+            limit_date = datetime.datetime(2100, 12, 31)
+
+        starting_date += datetime.timedelta(days=1)
+        limit_date += datetime.timedelta(days=1)
+        
+        with Session(bind=engine) as session:
             
-            session.add(new_document)
-            session.commit()
+            query_results_size = session.query(Document)\
+                .join(User, Document.id_register_user == User.id)\
+                .filter(
+                    Document.name.ilike(f'%{file_name}%'),
+                    User.name.ilike(f'%{user_register_name}%'),
+                    Document.register_date >= starting_date,
+                    Document.register_date <= limit_date
+                    )\
+                .where(Document.deleted.is_(False))\
+                .count()
+                
+            return query_results_size
+
+    @classmethod
+    def get_document_from_database(self, file_name, user_register_name, starting_date=None, limit_date=None, page=1):
+        
+        if starting_date is None:
+            starting_date = datetime.datetime(1900, 1, 1)
+        if limit_date is None:
+            limit_date = datetime.datetime(2100, 12, 31)
+        
+        starting_date += datetime.timedelta(days=1)
+        limit_date += datetime.timedelta(days=1)
+        
+        with Session(bind=engine) as session:
             
-            return True, new_document
-        except Exception as error:
-            session.rollback()
-            insert_system_log(error)
-        
-        
-def save_document_get_path(new_file):
-    
-    '''This funcion saves the file into the file system and returns the path to the file relative to the root folder.'''
-    
-    controllers_path = os.path.dirname(__file__)
-    src_path = os.path.dirname(controllers_path)
-    documents_path = os.path.join(src_path, "documents")
-    
-    # Create folder "documents" if not exists
-    os.makedirs(documents_path, exist_ok=True)
-    
-    file_path = os.path.join(documents_path, new_file.name)
-    
-    with open(file_path, "wb") as f:
-        f.write(new_file.read())
-    
-    
-    return os.path.relpath(file_path, src_path)
+            try:
+                
+                query_result = session.query(Document)\
+                    .join(User, Document.id_register_user == User.id)\
+                    .filter(
+                        Document.name.ilike(f'%{file_name}%'),
+                        User.name.ilike(f'%{user_register_name}%'),
+                        Document.register_date >= starting_date,
+                        Document.register_date <= limit_date
+                        )\
+                    .where(Document.deleted.is_(False))\
+                    .order_by(Document.register_date)\
+                    .slice(10 * (page-1), (10 * (page-1))+10)\
+                    .all()
+                    
+                commom_objects_result = [{
+                    'id': document.id, 'name': document.name,
+                    'id_register_user': document.id_register_user,
+                    'img': document.img,
+                    'tags':document.tags,
+                    'content': json.loads(document.content),
+                    'doc_history':document.log_document,
+                    'user_register': document.user_register.name,
+                    'register_date': document.register_date
+                    } for document in query_result]
+                
+                return commom_objects_result
+            except Exception as e:
+                session.rollback()
+                print(e)
+                return False
+     
+    @classmethod       
+    def soft_delete_document(self, document_id):
+        with Session(bind=engine) as session:
+            try:    
+                
+                document = session.query(Document)\
+                    .filter(Document.id == document_id).all()
+                
+                document[0].deleted = True   
 
-def get_document_from_database(name):
-    
-    with Session(bind=engine) as session:
-        
-        query_result = session.query(Document).filter(Document.name.like(f'%{name}%')).where(Document.deleted == False).order_by(Document.id).all()
-        
-        commom_objects_result = [{'id': document.id, 'name': document.name, 'type': document.type, 'id_register_user': document.id_register_user, 'img': document.img, 'tags':document.tags, 'content': document.content, 'doc_history':document.log_document} for document in query_result]
-        
-        return commom_objects_result
-        
+                session.commit()
+                return True
+            except Exception as e:
+                session.rollback()
+                print(e)
+                return False
         
 
-if __name__ == '__main__':
-    pass
+    @classmethod
+    def get_document_log_history(self, document_id):
+        
+        with Session(bind=engine) as session:
+            try:
+                query_result = session.query(LogDocument)\
+                .filter(LogDocument.id_document_modified==document_id)\
+                .all()
+                found_logs = [{'username': log.user.username, 'log_txt': log.log_txt, 'log_date': log.log_date} for log in query_result]
+
+                return found_logs
+            except Exception as e:
+                session.rollback()
+                print(e)
+                return False
